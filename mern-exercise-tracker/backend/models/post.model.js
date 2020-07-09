9efbb4f2db6225bc2a57b76f262fa97e737177f9
Aiden Burgess <aiden9874@gmail.com>
2020-07-09 22:06:12 +1200

    Create new schema for post

---
+++ mern-exercise-tracker/backend/models/post.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: false,
+		},
+		date: {
+			type: Date,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const post = mongoose.model("Post", postSchema);
+
+module.exports = post;
