f428ff705d15f6761ddca79fe25ee114eecaa945
Aiden Burgess <aiden9874@gmail.com>
2020-07-09 18:48:39 +1200

    Placeholder components created

---
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class CreateExercise extends Component {
+	render() {
+		return (
+			<div>
+				<p>You are on the Create Exercise component!</p>
+			</div>
+		);
+	}
+}
