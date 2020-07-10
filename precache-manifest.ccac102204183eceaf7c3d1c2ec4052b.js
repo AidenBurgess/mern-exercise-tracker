b956b66b4cb02a24cec1a74eea50b47295a9b6aa
Aiden Burgess <aiden9874@gmail.com>
2020-07-10 13:55:00 +1200

    Updates

---
+++ precache-manifest.ccac102204183eceaf7c3d1c2ec4052b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4132f93052b3ab0df8b98f50640b9d",
+    "url": "/AidenBurgess/MERN-exercise-tracker/index.html"
+  },
+  {
+    "revision": "44b8fb25d7188369adba",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/css/2.9861d4a4.chunk.css"
+  },
+  {
+    "revision": "f667cf1073e9b40a3347",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44b8fb25d7188369adba",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/js/2.a9ffe119.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/js/2.a9ffe119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f667cf1073e9b40a3347",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/js/main.385685f2.chunk.js"
+  },
+  {
+    "revision": "f066452d902a4cec504c",
+    "url": "/AidenBurgess/MERN-exercise-tracker/static/js/runtime-main.1dc5dead.js"
+  }
+]);
